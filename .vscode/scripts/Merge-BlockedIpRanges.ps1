b68b0039b8c1ac9d00c2b6803d37ac55874d2775
Timothy Small <smalls.online@icloud.com>
2025-04-23 09:44:07 -0400

    Define IP ranges in separate files
    These separate files will be merged into a final data file for ModSecurity.

---
+++ .vscode/scripts/Merge-BlockedIpRanges.ps1
@@ -0,0 +1,51 @@
+#!/usr/bin/env pwsh
+[CmdletBinding(SupportsShouldProcess)]
+param(
+    [Parameter(Position = 0)]
+    [ValidateNotNullOrEmpty()]
+    [string]$RootDirectory = "."
+)
+
+$resolvedRootDirectory = (Resolve-Path -Path $RootDirectory -ErrorAction "Stop").Path
+
+if ([System.IO.FileAttributes]::Directory -notin (Get-Item -Path $resolvedRootDirectory).Attributes) {
+    $PSCmdlet.ThrowTerminatingError(
+        [System.Management.Automation.ErrorRecord]::new(
+            [System.IO.IOException]::new("Supplied root directory path is not a directory."),
+            "InvalidRootDirectoryPath",
+            [System.Management.Automation.ErrorCategory]::InvalidArgument,
+            $resolvedRootDirectory
+        )
+    )
+}
+
+$ipRangesListsPath = Join-Path -Path $resolvedRootDirectory -ChildPath "blocked_ip_ranges"
+$ipRangesOutPath = Join-Path -Path $resolvedRootDirectory -ChildPath "modsecurity/blocked_ip_ranges.data"
+
+$ipRangesLists = Get-ChildItem -Path $ipRangesListsPath | Where-Object { $PSItem.Extension -in @(".yml", ".yaml") }
+
+$allIpRanges = [System.Collections.Generic.List[string]]::new()
+
+foreach ($ipRangeListItem in $ipRangesLists) {
+    $ipRangeDocument = Get-Content -Path $ipRangeListItem.FullName -Raw | ConvertFrom-Yaml
+
+    Write-Verbose -Message "Processing '$($ipRangeDocument["name"])'"
+
+    foreach ($rangeItem in $ipRangeDocument["ipRanges"]) {
+        $allIpRanges.Add($rangeItem)
+    }
+}
+
+$allIpRanges = $allIpRanges | Sort-Object
+
+$ipRangesStringBuilder = [System.Text.StringBuilder]::new()
+foreach ($rangeItem in $allIpRanges) {
+    $null = $ipRangesStringBuilder.AppendLine($rangeItem)
+}
+
+if ($PSCmdlet.ShouldProcess($ipRangesOutPath, "Update IP ranges data file")) {
+    $ipRangesStringBuilder.ToString() | Out-File -FilePath $ipRangesOutPath -Encoding "UTF8" -Force
+}
+else {
+    $ipRangesStringBuilder.ToString().TrimEnd() | Write-Output
+}
